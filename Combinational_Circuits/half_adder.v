d5a08a2fa3c99cd09628a8fc961f5a20e83c7f38
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-20 12:48:33 +0530

    Create half_adder.v

---
+++ Combinational_Circuits/half_adder.v
@@ -0,0 +1,14 @@
+module half_adder(input a,b, output sum, carry);
+  // RTL Design
+  // Data flow Model
+  
+    assign sum = a^b;
+    assign carry = a&b;
+endmodule
+
+
+/*
+// Gate level simulation
+xor(sum, a,b);
+and(carry, a,b);
+*/
