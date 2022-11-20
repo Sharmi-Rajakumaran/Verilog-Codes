60b0c1027d416976cc682f2881f314436312de1b
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-20 13:32:07 +0530

    Update 2_4_decoder.v
    Behavioural and data flow model of 2:4 Decoder

--- Combinational_Circuits/2_4_decoder.v
+++ Combinational_Circuits/2_4_decoder.v
@@ -1,12 +1,37 @@
-module 2_4_decoder(input [1:0]I, output [3:0] Y);
+module 2_4_decoder(input [1:0]i_I, output [3:0] o_Y);
   
   //Data Flow Abstraction
   
-  assign Y[0] = ~I[0] & ~I[1];
-  assign Y[1] = ~I[0] &  I[1];
-  assign Y[2] =  I[0] & ~I[1];
-  assign Y[3] =  I[0] &  I[1];
+  assign o_Y[0] = ~i_I[0] & ~i_I[1];
+  assign o_Y[1] = ~i_I[0] &  i_I[1];
+  assign o_Y[2] =  i_I[0] & ~i_I[1];
+  assign o_Y[3] =  i_I[0] &  i_I[1];
   
 endmodule
 
+
+/*
+
+Behavioural Code for 2:4 Decoder
+
+module decoder(in, out);
+
+input [1:0] in;
+output [3:0] out;
+
+reg [3:0] out;
+
+always@(in)
+case(in)
+2'd0: out = 4'd1;
+2'd1: out = 4'd2;
+2'd2: out = 4'd4;
+2'd3: out = 4'd8;
+
+default: out = 4'd0;
+endcase
+
+endmodule
+
+*/
 
